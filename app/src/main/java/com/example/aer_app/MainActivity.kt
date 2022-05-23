076b3c3916b59cb4cdbf0981657f43a181893922
Christian Yepes Gomez <christianyepesgomez@gmail.com>
2022-05-23 15:11:39 +0200

    Splash Screen added

--- app/src/main/java/com/example/aer_app/MainActivity.kt
+++ app/src/main/java/com/example/aer_app/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+        setTheme(R.style.Theme_AER_APP)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
