6170606f64a17780459955b55d5857c516a39e2a
Christian Yepes Gomez <christianyepesgomez@gmail.com>
2022-05-24 14:01:45 +0200

    - Drawer menu movement between fragments Done

--- app/src/main/java/com/example/aer_app/HomeActivity.kt
+++ app/src/main/java/com/example/aer_app/HomeActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.aer_app.databinding.ActivityAuthBinding
 import com.example.aer_app.databinding.ActivityHomeBinding
 import com.google.android.material.navigation.NavigationView
@@ -18,6 +19,7 @@ import org.w3c.dom.Text
 class HomeActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     private lateinit var binding: ActivityHomeBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
@@ -32,8 +34,9 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
+    //function to charge the Drawer menu and the items inside the menu
     fun chargeDrawer() {
-        val drawerLayout: DrawerLayout = binding.drawerLayout
+        drawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
 
         //Get the user_name textview and set the text to the current user name
@@ -49,25 +52,50 @@ class HomeActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
             when (it.itemId) {
-                R.id.nav_home -> Toast.makeText(applicationContext, "Home", Toast.LENGTH_SHORT)
-                    .show()
-                R.id.nav_logout -> {
-                    firebaseAuth.signOut()
-                    val intent = Intent(this, LogInActivity::class.java)
-                    startActivity(intent)
-                }
+                //Home
+                R.id.nav_home -> replaceFragment(HomeFragment(), it.title.toString())
+                //Users
+                R.id.nav_usuarios -> replaceFragment(UserFragment(), it.title.toString())
+                //Problems
+                R.id.nav_problemas -> replaceFragment(ProblemFragment(), it.title.toString())
+                //Institutions
+                R.id.nav_instituciones -> replaceFragment(
+                    InstitutionFragment(),
+                    it.title.toString()
+                )
+                //Setting
                 R.id.nav_settings -> Toast.makeText(
                     applicationContext,
                     "Settings",
                     Toast.LENGTH_SHORT
                 ).show()
+                //Share
                 R.id.nav_share -> Toast.makeText(applicationContext, "Share", Toast.LENGTH_SHORT)
                     .show()
+                //Logout
+                R.id.nav_logout -> {
+                    firebaseAuth.signOut()
+                    val intent = Intent(this, LogInActivity::class.java)
+                    startActivity(intent)
+                }
             }
 
             true
         }
+    }
+
+    //Function to replace view fragment with the clicked one
+    private fun replaceFragment(fragment: Fragment, title: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
